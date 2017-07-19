5d1e02a60b54db935999bf80b36af7c5a59d9c8f
Danning Peng <danningpeng@gmail.com>
2017-07-19 05:05:02 -0700

    Add files via upload

--- server/build/auction_server.js
+++ server/build/auction_server.js
@@ -4,17 +4,29 @@ var express = require("express");
 var ws_1 = require("ws");
 var app = express();
 var Product = (function () {
-    function Product(id, title, price, rating, desc, cateogories) {
+    function Product(id, title, price, rating, desc, categories) {
         this.id = id;
         this.title = title;
         this.price = price;
         this.rating = rating;
         this.desc = desc;
-        this.cateogories = cateogories;
+        this.categories = categories;
     }
     return Product;
 }());
 exports.Product = Product;
+var Comment = (function () {
+    function Comment(id, productId, timeStamp, user, rating, content) {
+        this.id = id;
+        this.productId = productId;
+        this.timeStamp = timeStamp;
+        this.user = user;
+        this.rating = rating;
+        this.content = content;
+    }
+    return Comment;
+}());
+exports.Comment = Comment;
 var products = [
     new Product(1, "Abby", 1.99, 3.5, "First Product xxxxxxxxxxxxxxxxxxxxxxxxxx", ["Electronics"]),
     new Product(2, "Bobby", 2.99, 2.2, "Second Product xxxxxxxxxxxxxxxxxxxxxxxxxx", ["Tools"]),
@@ -23,29 +35,72 @@ var products = [
     new Product(5, "Esty", 5.99, 2.8, "Fifth Product xxxxxxxxxxxxxxxxxxxxxxxxxx", ["Food", "Children"]),
     new Product(6, "Fitty", 6.99, 4.5, "Sixth Product xxxxxxxxxxxxxxxxxxxxxxxxxx", ["Electronics"])
 ];
+var comments = [
+    new Comment(1, 1, "2017-02-02", "april", 3, "ok"),
+    new Comment(2, 1, "2017-03-02", "may", 4, "good"),
+    new Comment(3, 1, "2017-04-02", "june", 2, "unsatisfied"),
+    new Comment(4, 2, "2017-05-02", "jane", 1, "bad")
+];
 app.get('/', function (req, res) {
     res.send("Hello Express!");
 });
-app.get('/products', function (req, res) {
-    res.json(products);
+app.get('/api/products', function (req, res) {
+    var result = products;
+    var params = req.query;
+    /*console.log("0");
+    console.log(result);
+    console.log(params);*/
+    if (params.title) {
+        result = result.filter(function (p) { return p.title.indexOf(params.title) !== -1; });
+    }
+    if (params.price && result.length > 0) {
+        result = result.filter(function (p) { return p.price <= parseInt(params.price); });
+    }
+    if (params.category && params.category !== "-1" && result.length > 0) {
+        result = result.filter(function (p) { return p.categories.indexOf(params.category) !== -1; });
+    }
+    console.log("3");
+    console.log(result);
+    res.json(result);
 });
-app.get('/products/:id', function (req, res) {
+app.get('/api/product/:id', function (req, res) {
     res.json(products.find(function (product) { return product.id == req.params.id; }));
+});
+app.get('/api/product/:id/comments', function (req, res) {
+    res.json(comments.filter(function (comment) { return comment.productId == req.params.id; }));
 });
 var server = app.listen(8000, "localhost", function () {
     console.log("server started at localhost:8000");
 });
+var subscriptions = new Map();
 var wsServer = new ws_1.Server({ port: 8085 });
-wsServer.on("connection", function (websocket) {
-    websocket.send("this is from the server");
-    websocket.on("message", function (message) {
-        console.log("message received" + message);
+wsServer.on('connection', function (websocket) {
+    //websocket.send('this is from the server');
+    websocket.on('message', function (message) {
+        console.log("message received");
+        console.log(message);
+        var messageObj = JSON.parse(message);
+        var productIds = subscriptions.get(websocket) || [];
+        subscriptions.set(websocket, productIds.concat([messageObj.productId]));
     });
 });
+var currentBids = new Map();
 setInterval(function () {
-    if (wsServer.clients) {
-        wsServer.clients.forEach(function (client) {
-            client.send("this is timely inquiry");
-        });
-    }
+    products.forEach(function (p) {
+        var currentBid = currentBids.get(p.id) || p.price;
+        var newBid = currentBid + Math.random() * 5;
+        currentBids.set(p.id, newBid);
+    });
+    subscriptions.forEach(function (productIds, ws) {
+        if (ws.readyState === 1) {
+            var newBids = productIds.map(function (pid) { return ({
+                productId: pid,
+                bid: currentBids.get(pid)
+            }); });
+            ws.send(JSON.stringify(newBids));
+        }
+        else {
+            subscriptions.delete(ws);
+        }
+    });
 }, 2000);
--- server/server/auction_server.ts
+++ server/server/auction_server.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import {Server} from 'ws';
-import {port} from "_debugger";
 
 const app = express();
 
@@ -11,7 +10,18 @@ export class Product {
         public price: number,
         public rating: number,
         public desc: string,
-        public cateogories: Array<string>) {
+        public categories: Array<string>) {
+    }
+}
+
+export class Comment {
+    constructor (
+        public id: number,
+        public productId: number,
+        public timeStamp: string,
+        public user: string,
+        public rating: number,
+        public content: string) {
     }
 }
 
@@ -24,34 +34,91 @@ const products: Product[] = [
     new Product(6, "Fitty", 6.99, 4.5, "Sixth Product xxxxxxxxxxxxxxxxxxxxxxxxxx", ["Electronics"])
 ];
 
+
+const comments: Comment[] = [
+    new Comment(1, 1, "2017-02-02", "april", 3, "ok"),
+    new Comment(2, 1, "2017-03-02", "may", 4, "good"),
+    new Comment(3, 1, "2017-04-02", "june", 2, "unsatisfied"),
+    new Comment(4, 2, "2017-05-02", "jane", 1, "bad")
+];
+
+
 app.get('/', (req, res) => {
     res.send("Hello Express!");
 });
 
-app.get('/products', (req, res) => {
-   res.json(products);
+app.get('/api/products', (req, res) => {
+    let result = products;
+    let params = req.query;
+
+    /*console.log("0");
+    console.log(result);
+    console.log(params);*/
+
+    if(params.title) {
+        result = result.filter((p) => p.title.indexOf(params.title) !== -1);
+    }
+
+    if(params.price && result.length > 0) {
+        result = result.filter((p) => p.price <= parseInt(params.price));
+    }
+
+    if(params.category && params.category !== "-1" && result.length > 0) {
+        result = result.filter((p) => p.categories.indexOf(params.category) !== -1);
+    }
+
+    console.log("3");
+    console.log(result);
+
+    res.json(result);
 });
 
-app.get('/products/:id', (req, res) => {
+
+app.get('/api/product/:id', (req, res) => {
     res.json(products.find((product) => product.id == req.params.id));
+});
+
+app.get('/api/product/:id/comments', (req, res) => {
+    res.json(comments.filter((comment: Comment) => comment.productId == req.params.id));
 });
 
 const server = app.listen(8000, "localhost", () => {
    console.log("server started at localhost:8000");
 });
 
+const subscriptions = new Map<any, number[]>();
+
 const wsServer = new Server({port : 8085});
-wsServer.on("connection", websocket => {
-    websocket.send("this is from the server");
-    websocket.on("message", message => {
-        console.log("message received" + message);
+wsServer.on('connection', websocket => {
+    //websocket.send('this is from the server');
+    websocket.on('message', message => {
+        console.log("message received");
+        console.log(message);
+        let messageObj = JSON.parse(message);
+        let productIds = subscriptions.get(websocket) || [];
+        subscriptions.set(websocket, [...productIds, messageObj.productId]);
     });
 });
 
+const currentBids = new Map<number, number>();
+
 setInterval(() => {
-    if(wsServer.clients) {
-        wsServer.clients.forEach(client => {
-            client.send("this is timely inquiry");
-        })
-    }
+
+    products.forEach(p => {
+        let currentBid = currentBids.get(p.id) || p.price;
+        let newBid = currentBid + Math.random()*5;
+        currentBids.set(p.id, newBid);
+    });
+
+    subscriptions.forEach((productIds: number[], ws) => {
+        if(ws.readyState === 1) {
+            let newBids = productIds.map( pid => ({
+                productId: pid,
+                bid: currentBids.get(pid)
+            }));
+            ws.send(JSON.stringify(newBids));
+        } else {
+            subscriptions.delete(ws);
+        }
+    });
 }, 2000);
